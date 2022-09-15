3c6413305cac98c94fe20b961ca320d826b145a2
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-09-16 00:44:43 +0900

    enable pip install

--- install_editable.sh
+++ install_editable.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-#pip install -e .
+pip install -e .
 find _skbuild/linux-*/cmake-install/qiskit_aer/backends -name "controller_wrappers*.so" -exec cp {} `pwd`/qiskit_aer/backends/ \;
 find _skbuild/linux-*/cmake-install/qiskit_aer/pulse/controllers -name "pulse_utils*.so" -exec cp {} `pwd`/qiskit_aer/pulse/controllers/ \;
 rm -rf `python -c "import site; print (site.getsitepackages()[0])"`/qiskit/providers/aer
