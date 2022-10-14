bfd337514ca8eeafd18102f81f22dd9c52e86bbc
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-10-15 01:53:39 +0900

    update install procedure

--- install_editable.sh
+++ install_editable.sh
@@ -3,5 +3,6 @@
 pip install -e .
 find _skbuild/linux-*/cmake-install/qiskit_aer/backends -name "controller_wrappers*.so" -exec cp {} `pwd`/qiskit_aer/backends/ \;
 find _skbuild/linux-*/cmake-install/qiskit_aer/pulse/controllers -name "pulse_utils*.so" -exec cp {} `pwd`/qiskit_aer/pulse/controllers/ \;
+find _skbuild/linux-*/cmake-build/lib/test_python_to_cpp*u.so  -exec cp {} `pwd`/qiskit_aer/pulse/controllers/ \;
 rm -rf `python -c "import site; print (site.getsitepackages()[0])"`/qiskit/providers/aer
 ln -s `pwd`/qiskit_aer `python -c "import site; print (site.getsitepackages()[0])"`/qiskit/providers/aer
