337769976dcf89de48aa7968b0c7d64028e7fbe5
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-10-29 00:10:36 +0900

    comment out needless procedure

--- install_editable.sh
+++ install_editable.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-pip install -e .
+#pip install -e .
 find _skbuild/linux-*/cmake-install/qiskit_aer/backends -name "controller_wrappers*.so" -exec cp {} `pwd`/qiskit_aer/backends/ \;
 find _skbuild/linux-*/cmake-install/qiskit_aer/pulse/controllers -name "pulse_utils*.so" -exec cp {} `pwd`/qiskit_aer/pulse/controllers/ \;
 find _skbuild/linux-*/cmake-build/lib/test_python_to_cpp*u.so  -exec cp {} `pwd`/qiskit_aer/pulse/controllers/ \;
