bac1ba0caac20476fe14ca09811d72f863f3546e
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-10-29 21:23:28 +0900

    proceed with implementation

--- qiskit_aer/quantum_info/states/aer_densitymatrix.py
+++ qiskit_aer/quantum_info/states/aer_densitymatrix.py
@@ -23,7 +23,7 @@ from qiskit_aer import AerSimulator
 from .aer_statevector import AerStatevector
 from .aer_state import AerState
 from ...backends.aerbackend import AerError
-from .aer_state import dbg_print
+
 
 class AerDensityMatrix(DensityMatrix):
     """AerDensityMatrix class
@@ -33,7 +33,17 @@ class AerDensityMatrix(DensityMatrix):
     def __init__(self, data, dims=None, **configs):
         """
         Args:
-            configs (kwargs): configurations of :class:`AerDensityMatrix`. `_aer_state` and `method` are valid.
+            data (np.array or list or AerStatevector or QuantumCircuit or
+                  qiskit.circuit.Instruction):
+                Data from which the densitymatrix can be constructed. This can be either a complex
+                vector, another densitymatrix or statevector or a ``QuantumCircuit`` or
+                ``Instruction`` (``Operator`` is not supportted in the current implementation).
+                If the data is a circuit or instruction, the densitymatrix is constructed by
+                assuming that all qubits are initialized to the zero state.
+            dims (int or tuple or list): Optional. The subsystem dimension of
+                                         the state (See additional information).
+            configs (kwargs): configurations of :class:`AerDensityMatrix`. `_aer_state` and `method`
+            are valid.
         Raises:
             AerError: if input data is not valid.
         Additional Information:
@@ -42,15 +52,16 @@ class AerDensityMatrix(DensityMatrix):
         if '_aer_state' in configs:
             self._aer_state = configs.pop('_aer_state')
         else:
-            #if 'method' not in configs:
-            #    configs['method'] = 'densitymatrix'
-            #elif configs['method'] not in ('densitymatrix'):
-            #    method = configs['method']
-            #    raise AerError(f'Method {method} is not supported')
+            if 'method' not in configs:
+                configs['method'] = 'density_matrix'
+            elif configs['method'] not in ('density_matrix'):
+                method = configs['method']
+                raise AerError(f'Method {method} is not supported')
             if isinstance(data, (QuantumCircuit, Instruction)):
                 data, aer_state = AerDensityMatrix._from_instruction(data, None, configs)
             elif isinstance(data, list):
-                data, aer_state = AerDensityMatrix._from_ndarray(data, configs)
+                data, aer_state = AerDensityMatrix._from_ndarray(np.array(data, dtype=complex),
+                                                                 configs)
             elif isinstance(data, np.ndarray):
                 data, aer_state = AerDensityMatrix._from_ndarray(data, configs)
             elif isinstance(data, AerDensityMatrix):
@@ -70,7 +81,8 @@ class AerDensityMatrix(DensityMatrix):
                 # If no 'to_operator' attribute exists we next look for a
                 # 'to_matrix' attribute to a matrix that will be cast into
                 # a complex numpy matrix.
-                data, aer_state = AerDensityMatrix._from_ndarray(np.asarray(data.to_matrix(), dtype=complex), configs)
+                data, aer_state = AerDensityMatrix._from_ndarray(
+                    np.asarray(data.to_matrix(), dtype=complex), configs)
             else:
                 raise AerError(f'Input data is not supported: type={data.__class__}, data={data}')
 
@@ -137,3 +149,56 @@ class AerDensityMatrix(DensityMatrix):
             AerStatevector._aer_evolve_instruction(aer_state, inst, range(num_qubits))
 
         return aer_state.move_to_ndarray(), aer_state
+
+    @classmethod
+    def from_label(cls, label):
+        r"""Return a tensor product of Pauli X,Y,Z eigenstates.
+        .. list-table:: Single-qubit state labels
+           :header-rows: 1
+           * - Label
+             - Statevector
+           * - ``"0"``
+             - :math:`\begin{pmatrix} 1 & 0 \\ 0 & 0 \end{pmatrix}`
+           * - ``"1"``
+             - :math:`\begin{pmatrix} 0 & 0 \\ 0 & 1 \end{pmatrix}`
+           * - ``"+"``
+             - :math:`\frac{1}{2}\begin{pmatrix} 1 & 1 \\ 1 & 1 \end{pmatrix}`
+           * - ``"-"``
+             - :math:`\frac{1}{2}\begin{pmatrix} 1 & -1 \\ -1 & 1 \end{pmatrix}`
+           * - ``"r"``
+             - :math:`\frac{1}{2}\begin{pmatrix} 1 & -i \\ i & 1 \end{pmatrix}`
+           * - ``"l"``
+             - :math:`\frac{1}{2}\begin{pmatrix} 1 & i \\ -i & 1 \end{pmatrix}`
+        Args:
+            label (string): a eigenstate string ket label (see table for
+                            allowed values).
+        Returns:
+            Statevector: The N-qubit basis state density matrix.
+        Raises:
+            QiskitError: if the label contains invalid characters, or the length
+                         of the label is larger than an explicitly specified num_qubits.
+        """
+        return AerDensityMatrix(AerStatevector.from_label(label))
+
+    @staticmethod
+    def from_int(i, dims):
+        """Return a computational basis state density matrix.
+        Args:
+            i (int): the basis state element.
+            dims (int or tuple or list): The subsystem dimensions of the statevector
+                                         (See additional information).
+        Returns:
+            DensityMatrix: The computational basis state :math:`|i\\rangle\\!\\langle i|`.
+        Additional Information:
+            The ``dims`` kwarg can be an integer or an iterable of integers.
+            * ``Iterable`` -- the subsystem dimensions are the values in the list
+              with the total number of subsystems given by the length of the list.
+            * ``Int`` -- the integer specifies the total dimension of the
+              state. If it is a power of two the state will be initialized
+              as an N-qubit state. If it is not a power of  two the state
+              will have a single d-dimensional subsystem.
+        """
+        size = np.product(dims)
+        state = np.zeros((size, size), dtype=complex)
+        state[i, i] = 1.0
+        return AerDensityMatrix(state, dims=dims)
