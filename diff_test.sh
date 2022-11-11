de18bec1dad1d0f7ee53feed6de7749fd68cbbfc
derwind <uncertainty_principle_ss@yahoo.co.jp>
2022-11-11 23:15:35 +0900

    fix typo

--- diff_test.sh
+++ diff_test.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
 cp test/terra/states/test_aer_densitymatrix.py test/terra/states/test_aer_densitymatrix_.py
-perl -pi -e 's/AerDensityMatrix/DensityMatrix' test/terra/states/test_aer_densitymatrix_.py
+perl -pi -e 's/AerDensityMatrix/DensityMatrix/g; s/AerStatevector/Statevector/g' test/terra/states/test_aer_densitymatrix_.py
 diff -u ../qiskit-terra/test/python/quantum_info/states/test_densitymatrix.py test/terra/states/test_aer_densitymatrix_.py
